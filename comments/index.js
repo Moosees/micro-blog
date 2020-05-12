7b2cf1675941bb978fce0ccc6420c9d3818ef2d1
Moosees <linus.almgren@gmail.com>
2020-05-12 04:44:06 +0200

    setup express for comments

---
+++ comments/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const port = 4001;
+const app = express();
+app.use(bodyParser.json());
+
+// hold our comment data for now
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const { content } = req.body;
+  const { id } = req.params;
+  const newId = randomBytes(4).toString('hex');
+
+  const comments = commentsByPostId[id] || [];
+
+  comments.push({ id: newId, content });
+
+  commentsByPostId[id] = comments;
+
+  res.status(201).send(commentsByPostId[id]);
+});
+
+app.listen(port, () => {
+  console.log(`Running COMMENTS service on port ${port}`);
+});
